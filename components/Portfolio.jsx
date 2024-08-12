ac297baafb92af1424bfa9fc059fa7bffe46e34a
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-08-12 12:41:10 +0200

    chore: Update portfolio page layout and component styles

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import PortfolioLImageItem from './portfolio/PortfolioLImageItem'
+import PortfolioRImageItem from './portfolio/PortfolioRImageItem'
 
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+   <section className="px-32 py-12">
+       <h3 className="pb-12 text-7xl font-bold lg:text-5xl text-center  ">Portfolio</h3>
+       <PortfolioLImageItem 
+       title="U4ric Sports"
+       imageLink="/portfolio/u4ric.png"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut imperdiet justo. Suspendisse pretium, lorem sed tempor eleifend, arcu sem cursus nulla, sed mollis ligula velit quis urna. Nulla dapibus eget est non aliquet. Sed quis massa in tortor efficitur finibus quis at ex. Praesent euismod metus ac suscipit vestibulum. Mauris at luctus diam. Fusce ultrices purus ac mi fringilla sodales. In hac habitasse platea dictumst. Morbiåß vel turpis scelerisque mi volutpat euismod. Maecenas id consequat nisl."
+       />
+       <PortfolioRImageItem
+             title="Mother Touch Group of Schools"
+             imageLink="/portfolio/mtgs.png"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut imperdiet justo. Suspendisse pretium, lorem sed tempor eleifend, arcu sem cursus nulla, sed mollis ligula velit quis urna. Nulla dapibus eget est non aliquet. Sed quis massa in tortor efficitur finibus quis at ex. Praesent euismod metus ac suscipit vestibulum. Mauris at luctus diam. Fusce ultrices purus ac mi fringilla sodales. In hac habitasse platea dictumst. Morbiåß vel turpis scelerisque mi volutpat euismod. Maecenas id consequat nisl."
+       />
+       <PortfolioLImageItem 
+        title="USSD-Based Payment Integration for Content Delivery Platform"
+        imageLink="/portfolio/sdp.png"
+         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut imperdiet justo. Suspendisse pretium, lorem sed tempor eleifend, arcu sem cursus nulla, sed mollis ligula velit quis urna. Nulla dapibus eget est non aliquet. Sed quis massa in tortor efficitur finibus quis at ex. Praesent euismod metus ac suscipit vestibulum. Mauris at luctus diam. Fusce ultrices purus ac mi fringilla sodales. In hac habitasse platea dictumst. Morbiåß vel turpis scelerisque mi volutpat euismod. Maecenas id consequat nisl."
+       />
+
+        <div className="flex justify-center py-10">
+          <button class="btn btn-warning bg-green-700 text-white border-green-700">View More Projects</button>
+        </div>
+
+   </section>
   )
 }
---
+++ components/portfolio/PortfolioLImageItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function PortfolioLImageItem({title, imageLink, description, link }) {
+  return (
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 py-8">
+    <div className="rounded-lg ">
+        <Image src={imageLink} alt={title} width={500} height={500} />
+    </div>
+    <div className="rounded-lg">
+        <h3 className="py-4 text-7xl font-semibold lg:text-2xl">{title}</h3>
+        <p className="text-gray-500">{description} </p>
+        <button className="btn btn-outline btn-success mt-10 px-20">Details</button>
+    </div>
+  </div>
+  )
+}

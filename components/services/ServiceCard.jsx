adfa61399d4c8b8ec742d5b464ae377daca97d5b
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-09-02 18:43:07 +0200

    style: Update ServiceCard component styles

--- components/services/ServiceCard.jsx
+++ components/services/ServiceCard.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 export default function ServiceCard({icon, title, description}) {
   return (
-    <div className="border rounded-lg bg-gradient-to-bl from-gray-50 to-teal-50 py-10 bg-opacity-20 ">
+    <div className="border rounded-lg backdrop-blur-sm bg-gradient-to-bl from-gray-50 to-teal-50 py-10 bg-opacity-20 ">
     <div className="flex justify-center items-center text-6xl py-4">
 
           {icon}
     </div>
-    <h2 className="mb-4  text-xl tracking-tight font-semibold text-center text-gray-900 dark:text-white">{title}</h2>
+    <h2 className="mb-4  text-xl tracking-tight font-semibold text-center text-gray-900 ">{title}</h2>
     <p className="text-center mx-4">{description}</p>
   </div>
   )

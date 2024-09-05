8f898d2e18c4c2c33cc080180637805b2800a474
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-09-05 14:22:51 +0200

    refactor: Update ServiceCard component styles and hover effect
    This commit updates the ServiceCard component styles and adds a hover effect with opacity transition. The absolute positioning, top, right, and margin properties are modified to achieve the desired visual effect. The text color of the hover effect is changed to green. This change enhances the visual appeal and interactivity of the ServiceCard component.

--- components/services/ServiceCard.jsx
+++ components/services/ServiceCard.jsx
@@ -4,7 +4,7 @@ export default function ServiceCard({ icon, title, description }) {
   return (
     <div className="border border-gray-500 border-x-0 border-t-0 hover:border-b-green-400  bg-gray-600 py-10 bg-opacity-20  text-gray-300 relative overflow-hidden group"> 
       {/* Added 'group' for hover effect */}
-      <div className="absolute top-0 right-0 -mt-8 -mr-4 text-9xl text-gray-400 opacity-0 group-hover:opacity-15 transition-opacity duration-500"> 
+      <div className="absolute top-0 right-0 -mt-8 -mr-4 text-9xl text-green-200 opacity-0 group-hover:opacity-10 transition-opacity duration-500"> 
         {/* Added hover effect with opacity transition */}
         {icon} 
       </div>

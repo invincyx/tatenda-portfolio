b274f99235ec2456d5b9aaaf8415cdeaa51105cb
Tatenda <40611131+invincyx@users.noreply.github.com>
2024-09-05 15:56:25 +0200

    refactor: Add lazy loading for images and initialize Google Analytics
    This commit adds lazy loading for images in the `page.jsx` file to improve page load performance. It also initializes Google Analytics with the provided tracking ID in the `Home` component. This change enhances the user experience by optimizing image loading and enables tracking of page views with Google Analytics.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']}); 
@@ -12,6 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-9CLN8CJN8C"></Script>
+          <Script id="google-analytics">
+            {`
+             window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-9CLN8CJN8C');
+            `}
+          </Script>
+      </head>
+
       <body className={poppins.className}>{children}</body>
     </html>
   );
